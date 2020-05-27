1ee9c018520a17bab965127be9e345704976548b
Stanislav <stanislav.drakin@gmail.com>
2020-05-27 23:08:03 +0300

    up

--- src/app.core.js
+++ src/app.core.js
@@ -2,6 +2,29 @@ import initPlugins from './components/core/initplugins'
 
 export default {
 
+	whichTransitionEvent() {
+		let el = document.createElement('fakeelement');
+
+		let transitions = {
+			'transition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'MozTransition': 'transitionend',
+			'WebkitTransition': 'webkitTransitionEnd'
+		}
+
+		for (let t in transitions) {
+			if (el.style[t] !== void 0) {
+				return transitions[t];
+			}
+		}
+
+		return null;
+	},
+
+	keygen(prefix) {
+		return (prefix ? (prefix + '-') : '') + Math.random().toString(36).substr(2, 10);
+	},
+
 	init(options = {}) {
 
 		if (options.modules) {
